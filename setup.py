4a663dcd650e217d4e67533e687e62619bb08c8f
Yoshi2 <debugyoshi@gmail.com>
2019-11-24 10:39:11 +0100

    modify build script for mkdd editor

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import sys
 import os 
 from cx_Freeze import setup, Executable
-version = "1.0"
+version = "0.5"
 # Dependencies are automatically detected, but it might need fine tuning.
 
 def files_from_folder(folder):
@@ -9,12 +9,13 @@ def files_from_folder(folder):
 
 #include_files = files_from_folder("resources/")
 #include_files.extend(files_from_folder("object_templates"))
-include_files = ["resources/", "object_templates/"]
+include_files = ["resources/", "lib/mkddobjects.json", "lib/music_ids.json"]
 build_exe_options = {
-"packages": ["OpenGL", "numpy.core._methods", "numpy.lib.format"], 
+"packages": ["OpenGL", "numpy.core._methods", "numpy.lib.format"],
+"includes": ["widgets"], 
 "excludes": ["tkinter", "scipy", "PyQt5.QtWebEngine", "PyQt5.QtWebEngineCore"],
 "optimize": 0,
-"build_exe": "build/piktools-{}".format(version),
+"build_exe": "build/mkdd-track-editor-{}".format(version),
 "include_files": include_files}
 
 # GUI applications require a different base on Windows (the default is for a
@@ -24,8 +25,8 @@ guiBase = None#"Win32GUI"
 #if sys.platform == "win32":
 #    base = "Win32GUI"
 
-setup(  name = "Pikmin 3 GUI Tools",
+setup(  name = "MKDD Track Editor",
         version = version,
-        description = "GUI Tools for Pikmin 3",
+        description = "Track Editor for MKDD",
         options={"build_exe": build_exe_options},
-        executables = [Executable("pikmingen_editor.py", base=guiBase, icon="resources/icon.ico")])
+        executables = [Executable("mkdd_editor.py", base=guiBase, icon="resources/icon.ico")])
