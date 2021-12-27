7b6adea12f006291e80e65856e5fa4b0b64d30d6
cristian64 <cristian64@gmail.com>
2021-12-27 22:21:26 +0000

    Enhanced enemy points representation with extra colors when extra fields are provided.

--- mkdd_widgets.py
+++ mkdd_widgets.py
@@ -809,6 +809,22 @@ class BolMapViewer(QtWidgets.QOpenGLWidget):
                             self.models.draw_sphere(point.position, point.scale)
                         self.models.render_generic_position_colored(point.position, point in select_optimize, "enemypoint")
 
+                        if point.pointsetting:
+                            glColor3f(0.9, 0.0, 0.1)
+                            self.models.draw_cylinder(point.position, 700, 700)
+                        if point.groupsetting:
+                            glColor3f(0.1, 0.9, 0.2)
+                            self.models.draw_cylinder(point.position, 600, 600)
+                        if point.pointsetting2:
+                            glColor3f(0.2, 0.2, 0.9)
+                            self.models.draw_cylinder(point.position, 500, 500)
+                        if point.unk1:
+                            glColor3f(0.9, 0.9, 0.1)
+                            self.models.draw_cylinder(point.position, 400, 400)
+                        if point.unk2:
+                            glColor3f(0.9, 0.0, 0.9)
+                            self.models.draw_cylinder(point.position, 300, 300)
+
                     glBegin(GL_LINE_STRIP)
                     glColor3f(0.0, 0.0, 0.0)
                     for point in group.points:
