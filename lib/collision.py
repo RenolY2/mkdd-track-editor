RenolY2/mkdd-track-editor

commit 0e7a34c37b1e158ad5e0f5276f58389999e34fef

Reimplement 3D ray collision with Numba.

lib/collision.py
@@ -1,3 +1,8 @@
+import math
+
+import numba
+import numpy
+
 from .vectors import Vector3, Triangle, Line
 
 
@@ -19,6 +24,12 @@ class Collision:
             if not triangle.normal.is_zero():
                 self.triangles.append(triangle)
 
+        self.flat_triangles = []
+        for t in self.triangles:
+            self.flat_triangles.extend((t.origin.x, t.origin.y, t.origin.z, t.p2.x, t.p2.y, t.p2.z,
+                                        t.p3.x, t.p3.y, t.p3.z))
+        self.flat_triangles = numpy.array(self.flat_triangles)
+
     def collide_ray_downwards(self, x, z, y=99999999):
         result = self.collide_ray(Line(Vector3(x, -z, y), Vector3(0.0, 0.0, -1.0)))
         return result.z if result is not None else None
@@ -39,7 +50,7 @@ class Collision:
         dist2 = abs(y - result2.z)
         return result2.z if dist1 > dist2 else result1.z
 
-    def collide_ray(self, ray):
+    def collide_ray_legacy(self, ray):
         best_distance = None
         place_at = None
 
@@ -54,3 +65,160 @@ class Collision:
                     best_distance = distance
 
         return place_at
+
+    def collide_ray(self, ray):
+        place_at = _collide_ray_and_triangles(
+            ray.origin.x,
+            ray.origin.y,
+            ray.origin.z,
+            ray.direction.x,
+            ray.direction.y,
+            ray.direction.z,
+            self.flat_triangles,
+        )
+
+        if math.isnan(place_at[0]):
+            return None
+
+        return Vector3(*place_at)
+
+
+@numba.jit(nopython=True, nogil=True, cache=True)
+def cross(
+    x0: float,
+    y0: float,
+    z0: float,
+    x1: float,
+    y1: float,
+    z1: float,
+) -> tuple[float, float, float]:
+    return y0 * z1 - z0 * y1, z0 * x1 - x0 * z1, x0 * y1 - y0 * x1
+
+
+@numba.jit(nopython=True, nogil=True, cache=True)
+def dot(
+    x0: float,
+    y0: float,
+    z0: float,
+    x1: float,
+    y1: float,
+    z1: float,
+) -> tuple[float, float, float]:
+    return x0 * x1 + y0 * y1 + z0 * z1
+
+
+@numba.jit(nopython=True, nogil=True, cache=True)
+def length(x: float, y: float, z: float) -> float:
+    return math.sqrt(x * x + y * y + z * z)
+
+
+@numba.jit(nopython=True, nogil=True, cache=True)
+def normal(
+    x0: float,
+    y0: float,
+    z0: float,
+    x1: float,
+    y1: float,
+    z1: float,
+    x2: float,
+    y2: float,
+    z2: float,
+) -> tuple[float, float, float]:
+    x, y, z = cross(x2 - x0, y2 - y0, z2 - z0, x1 - x0, y1 - y0, z1 - z0)
+    size = length(x, y, z)
+    x /= size
+    y /= size
+    z /= size
+    return -x, -y, -z
+
+
+@numba.jit(nopython=True, nogil=True, cache=True)
+def subtract(
+    x0: float,
+    y0: float,
+    z0: float,
+    x1: float,
+    y1: float,
+    z1: float,
+) -> tuple[float, float, float]:
+    return x0 - x1, y0 - y1, z0 - z1
+
+
+@numba.jit(nopython=True, nogil=True, cache=True)
+def _collide_ray_and_triangle(
+    x: float,
+    y: float,
+    z: float,
+    dx: float,
+    dy: float,
+    dz: float,
+    x0: float,
+    y0: float,
+    z0: float,
+    x1: float,
+    y1: float,
+    z1: float,
+    x2: float,
+    y2: float,
+    z2: float,
+):
+    nx, ny, nz = normal(x0, y0, z0, x1, y1, z1, x2, y2, z2)
+
+    d = dot(nx, ny, nz, dx, dy, dz)
+    if d == 0.0:
+        return 0.0, 0.0, 0.0, 0.0
+
+    d = dot(*subtract(x0, y0, z0, x, y, z), nx, ny, nz) / d
+    if d < 0.0:
+        return 0.0, 0.0, 0.0, 0.0
+
+    intersection_point = x + dx * d, y + dy * d, z + dz * d
+
+    C0 = intersection_point[0] - x0, intersection_point[1] - y0, intersection_point[2] - z0
+    if dot(nx, ny, nz, *cross(*subtract(x1, y1, z1, x0, y0, z0), *C0)) > 0.0:
+        C1 = intersection_point[0] - x1, intersection_point[1] - y1, intersection_point[2] - z1
+        if dot(nx, ny, nz, *cross(*subtract(x2, y2, z2, x1, y1, z1), *C1)) > 0.0:
+            C2 = intersection_point[0] - x2, intersection_point[1] - y2, intersection_point[2] - z2
+            if dot(nx, ny, nz, *cross(*subtract(x0, y0, z0, x2, y2, z2), *C2)) > 0.0:
+                return d, *intersection_point
+
+    return 0.0, 0.0, 0.0, 0.0
+
+
+@numba.jit(nopython=True, nogil=True, cache=True)
+def _collide_ray_and_triangles(
+    x: float,
+    y: float,
+    z: float,
+    dx: float,
+    dy: float,
+    dz: float,
+    triangles: numpy.array,
+) -> tuple[float, float, float]:
+    collisions = []
+    for t in range(len(triangles) // 9):
+        collision = _collide_ray_and_triangle(
+            x,
+            y,
+            z,
+            dx,
+            dy,
+            dz,
+            triangles[t * 9 + 0],
+            triangles[t * 9 + 1],
+            triangles[t * 9 + 2],
+            triangles[t * 9 + 3],
+            triangles[t * 9 + 4],
+            triangles[t * 9 + 5],
+            triangles[t * 9 + 6],
+            triangles[t * 9 + 7],
+            triangles[t * 9 + 8],
+        )
+
+        if collision[0] > 0.0:
+            collisions.append(collision)
+
+    if collisions:
+        return min(collisions)[1:]
+
+    return math.nan, math.nan, math.nan

commit c25485d5369dfe418cbc491bb73e20465a0f4cc2

Accept `0.0` as a valid triangle collision distance.
This was a regression with the new Numba-base triangle collision
implementation, introduced in 0e7a34c37b1e.

`0.0` was inadvertently used to indicate "no collision", but that is a
valid value when the ray origin is contained in the triangle. Grounding
objects that were already grounded could result in objects being
grounded to a much higher or lower triangle, as the closest triangle was
wrongly disregarded.

`-1.0` is now used. (Note that Numba is strongly typed; returning `None`
or other rich type is not possible.)

lib/collision.py
@@ -207,11 +207,11 @@ def _collide_ray_and_triangle(
 
     d = dot(nx, ny, nz, dx, dy, dz)
     if d == 0.0:
-        return 0.0, 0.0, 0.0, 0.0
+        return -1.0, 0.0, 0.0, 0.0
 
     d = dot(*subtract(x0, y0, z0, x, y, z), nx, ny, nz) / d
     if d < 0.0:
-        return 0.0, 0.0, 0.0, 0.0
+        return -1.0, 0.0, 0.0, 0.0
 
     intersection_point = x + dx * d, y + dy * d, z + dz * d
 
@@ -223,7 +223,7 @@ def _collide_ray_and_triangle(
             if dot(nx, ny, nz, *cross(*subtract(x0, y0, z0, x2, y2, z2), *C2)) > 0.0:
                 return d, *intersection_point
 
-    return 0.0, 0.0, 0.0, 0.0
+    return -1.0, 0.0, 0.0, 0.0
 
 
 @numba.jit(nopython=True, nogil=True, cache=True)
@@ -256,7 +256,7 @@ def _collide_ray_and_triangles(
             triangles[t * 9 + 8],
         )
 
-        if collision[0] > 0.0:
+        if collision[0] >= 0.0:
             collisions.append(collision)
 
     if collisions: