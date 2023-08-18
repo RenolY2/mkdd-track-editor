RenolY2/mkdd-track-editor

commit 016f5885dca197b9fbef86b0e64d5e2aa7e31f57

dropdown menus for choosing camera/routes in objects/areas/cameras

mkdd_editor.py
@@ -2677,7 +2677,7 @@ class GenEditor(QtWidgets.QMainWindow):
                     objects = []
                     index = self.level_file.routes.index(currentobj)
                     for object in self.level_file.objects.objects:
-                        if object.pathid == index:
+                        if object.route == currentobj:
                             objects.append(get_full_name(object.objectid))
                     for i, camera in enumerate(self.level_file.cameras):
                         if camera.route == index:

lib/libbol.py
@@ -211,12 +211,12 @@ class ObjectContainer(list):
         self.object_type = object_type
 
     @classmethod
-    def from_file(cls, f, count, objcls):
+    def from_file(cls, f, count, objcls, *args):
         container = cls()
         container.object_type = objcls
 
         for i in range(count):
-            obj = objcls.from_file(f)
+            obj = objcls.from_file(f, *args)
             container.append(obj)
 
         return container
@@ -736,7 +736,7 @@ class MapObject(object):
         self.scale = Vector3(1.0, 1.0, 1.0)
         self.rotation = Rotation.default()
         self.objectid = objectid
-        self.pathid = -1
+        self.route = None
         self.unk_28 = 0
         self.unk_2a = -1
         self.presence_filter = 143
@@ -752,7 +752,7 @@ class MapObject(object):
         return cls(Vector3(0.0, 0.0, 0.0), 1)
 
     @classmethod
-    def from_file(cls, f):
+    def from_file(cls, f, routes: ObjectContainer):
         start = f.tell()
         position = Vector3(*unpack(">fff", f.read(12)))
         scale = Vector3(*unpack(">fff", f.read(12)))
@@ -764,7 +764,17 @@ class MapObject(object):
         obj = MapObject(position, objectid)
         obj.scale = scale
         obj.rotation = Rotation.from_mkdd_rotation(fx, fy, fz, ux, uy, uz)
-        obj.pathid = read_int16(f)
+        pathid = read_int16(f)
+
+        if pathid < 0:
+            obj.route = None
+        else:
+            try:
+                obj.route = routes[pathid]
+            except IndexError:
+                print("Object", objectid, "had an invalid route id")
+                obj.route = None
+
         obj.unk_28 = read_uint16(f)
         obj.unk_2a = read_int16(f)
         obj.presence_filter = read_uint8(f)
@@ -781,13 +791,21 @@ class MapObject(object):
         obj._size = f.tell() - start
         return obj
 
-    def write(self, f):
+    def write(self, f, routes: ObjectContainer):
         start = f.tell()
         f.write(pack(">fff", self.position.x, self.position.y, self.position.z))
         f.write(pack(">fff", self.scale.x, self.scale.y, self.scale.z))
         self.rotation.write(f)
 
-        f.write(pack(">hhHh", self.objectid, self.pathid, self.unk_28, self.unk_2a))
+        if self.route is None:
+            routeid = -1
+        else:
+            try:
+                routeid = routes.index(self.route)
+            except ValueError:
+                routeid = -1
+
+        f.write(pack(">hhHh", self.objectid, routeid, self.unk_28, self.unk_2a))
         f.write(pack(">BBBB", self.presence_filter, self.presence, self.unk_flag, self.unk_2f))
 
         for i in range(8):
@@ -807,6 +825,7 @@ class MapObject(object):
         json_data = self.read_json_file()
         return json_data.get("DefaultValues") if json_data is not None else None
 
+
 class MapObjects(object):
     def __init__(self):
         self.objects = []
@@ -816,11 +835,11 @@ class MapObjects(object):
         self.objects = []
 
     @classmethod
-    def from_file(cls, f, objectcount):
+    def from_file(cls, f, objectcount, routes: ObjectContainer):
         mapobjs = cls()
 
         for i in range(objectcount):
-            obj = MapObject.from_file(f)
+            obj = MapObject.from_file(f, routes)
             mapobjs.objects.append(obj)
 
         return mapobjs
@@ -916,7 +935,8 @@ class Area(object):
         self.rotation = Rotation.default()
         self.shape = 0
         self.area_type = 0
-        self.camera_index = -1
+        self.camera = None
+        self._cameraindex = -1
         self.feather = Feather()
         self.unkfixedpoint = 0
         self.unkshort = 0
@@ -938,7 +958,8 @@ class Area(object):
         area.rotation = Rotation.from_file(f)
         area.shape = read_uint8(f)
         area.area_type = read_uint8(f)
-        area.camera_index = read_int16(f)
+        area._cameraindex = read_int16(f)
+
         area.feather.i0 = read_uint32(f)
         area.feather.i1 = read_uint32(f)
         area.unkfixedpoint = read_int16(f)
@@ -951,11 +972,29 @@ class Area(object):
 
         return area
 
-    def write(self, f):
+    def setcam(self, cameras: ObjectContainer):
+        if self._cameraindex < 0:
+            self.camera = None
+        else:
+            try:
+                self.camera = cameras[self._cameraindex]
+            except ValueError:
+                self.camera = None
+
+    def write(self, f, cameras: ObjectContainer):
         f.write(pack(">fff", self.position.x, self.position.y, self.position.z))
         f.write(pack(">fff", self.scale.x, self.scale.y, self.scale.z))
         self.rotation.write(f)
-        f.write(pack(">BBh", self.shape, self.area_type, self.camera_index))
+
+        if self.camera is None:
+            camera_index = -1
+        else:
+            try:
+                camera_index = cameras.index(self.camera)
+            except ValueError:
+                camera_index = -1
+
+        f.write(pack(">BBh", self.shape, self.area_type, camera_index))
         f.write(pack(">II", self.feather.i0, self.feather.i1))
         f.write(pack(">hhhh", self.unkfixedpoint, self.unkshort, self.shadow_id, self.lightparam_index))
 
@@ -999,9 +1038,10 @@ class Camera(object):
         self.camduration = 0
         self.startcamera = 0
         self.shimmer = Shimmer()
-        self.route = -1
+        self.route = None
         self.routespeed = 0
-        self.nextcam = -1
+        self.nextcam = None
+        self._nextcam = -1
         self.name = "null"
 
         self.widget = None
@@ -1011,7 +1051,7 @@ class Camera(object):
         return cls(Vector3(0.0, 0.0, 0.0))
 
     @classmethod
-    def from_file(cls, f):
+    def from_file(cls, f, routes: ObjectContainer):
         position = Vector3(*unpack(">fff", f.read(12)))
 
         cam = cls(position)
@@ -1024,23 +1064,60 @@ class Camera(object):
         cam.startcamera = read_uint16(f)
         cam.shimmer.z0 = read_uint16(f)
         cam.shimmer.z1 = read_uint16(f)
-        cam.route = read_int16(f)
+
+        pathid = read_int16(f)
+
+        if pathid < 0:
+            cam.route = None
+        else:
+            try:
+                cam.route = routes[pathid]
+            except IndexError:
+                print("Camera had an invalid route id")
+                cam.route = None
+
         cam.routespeed = read_uint16(f)
         cam.fov.end = read_uint16(f)
-        cam.nextcam = read_int16(f)
+        cam._nextcam = read_int16(f)
         cam.name = str(f.read(4), encoding="ascii")
 
         return cam
 
-    def write(self, f):
+    def setnextcam(self, cameras: ObjectContainer):
+        if self._nextcam < 0:
+            self.nextcam = None
+        else:
+            try:
+                self.nextcam = cameras[self._nextcam]
+            except ValueError:
+                self.nextcam = None
+
+    def write(self, f, routes: ObjectContainer, cameras: ObjectContainer):
         f.write(pack(">fff", self.position.x, self.position.y, self.position.z))
         self.rotation.write(f)
         f.write(pack(">fff", self.position2.x, self.position2.y, self.position2.z))
         f.write(pack(">fff", self.position3.x, self.position3.y, self.position3.z))
         f.write(pack(">HHHH", self.camtype, self.fov.start, self.camduration, self.startcamera))
+
+        if self.route is None:
+            routeid = -1
+        else:
+            try:
+                routeid = routes.index(self.route)
+            except ValueError:
+                routeid = -1
+
+        if self.nextcam is None:
+            nextcamid = -1
+        else:
+            try:
+                nextcamid = cameras.index(self.nextcam)
+            except ValueError:
+                nextcamid = -1
+
         f.write(pack(">HHhHHh",
-                     self.shimmer.z0, self.shimmer.z1, self.route,
-                     self.routespeed, self.fov.end, self.nextcam))
+                     self.shimmer.z0, self.shimmer.z1, routeid,
+                     self.routespeed, self.fov.end, nextcamid))
         assert len(self.name) == 4
         f.write(bytes(self.name, encoding="ascii"))
 
@@ -1307,7 +1384,7 @@ class BOL(object):
             route.add_routepoints(routepoints)
 
         f.seek(sectionoffsets[OBJECTS])
-        bol.objects = MapObjects.from_file(f, sectioncounts[OBJECTS])
+        bol.objects = MapObjects.from_file(f, sectioncounts[OBJECTS], bol.routes)
 
         f.seek(sectionoffsets[KARTPOINT])
         bol.kartpoints = KartStartPoints.from_file(f, (sectionoffsets[AREA] - sectionoffsets[KARTPOINT])//0x28)
@@ -1325,7 +1402,12 @@ class BOL(object):
         bol.areas = Areas.from_file(f, sectioncounts[AREA])
 
         f.seek(sectionoffsets[CAMERA])
-        bol.cameras = ObjectContainer.from_file(f, sectioncounts[CAMERA], Camera)
+        bol.cameras = ObjectContainer.from_file(f, sectioncounts[CAMERA], Camera, bol.routes)
+        for camera in bol.cameras:
+            camera.setnextcam(bol.cameras)
+
+        for area in bol.areas.areas:
+            area.setcam(bol.cameras)
 
         f.seek(sectionoffsets[RESPAWNPOINT])
         bol.respawnpoints = ObjectContainer.from_file(f, sectioncounts[RESPAWNPOINT], JugemPoint)
@@ -1408,7 +1490,7 @@ class BOL(object):
 
         offsets.append(f.tell())
         for object in self.objects.objects:
-            object.write(f)
+            object.write(f, self.routes)
 
         offsets.append(f.tell())
         for startpoint in self.kartpoints.positions:
@@ -1416,11 +1498,11 @@ class BOL(object):
 
         offsets.append(f.tell())
         for area in self.areas.areas:
-            area.write(f)
+            area.write(f, self.cameras)
 
         offsets.append(f.tell())
         for camera in self.cameras:
-            camera.write(f)
+            camera.write(f, self.routes, self.cameras)
 
         offsets.append(f.tell())
         for respawnpoint in self.respawnpoints:

mkdd_widgets.py
@@ -744,14 +744,14 @@ class BolMapViewer(QtOpenGLWidgets.QOpenGLWidget):
 
                 if selectable_objectroutes or selectable_cameraroutes or selectable_unassignedroutes:
                     camera_routes = set(camera.route for camera in self.level_file.cameras)
-                    object_routes = set(obj.pathid for obj in self.level_file.objects.objects)
+                    object_routes = set(obj.route for obj in self.level_file.objects.objects)
                     assigned_routes = camera_routes.union(object_routes)
                     i = 0
-                    for idx, route in enumerate(self.level_file.routes):
+                    for route in self.level_file.routes:
                         for obj in route.points:
-                            if (not ((idx in object_routes and selectable_objectroutes) or
-                                     (idx in camera_routes and selectable_cameraroutes) or
-                                     (idx not in assigned_routes and selectable_unassignedroutes))):
+                            if (not ((route in object_routes and selectable_objectroutes) or
+                                     (route in camera_routes and selectable_cameraroutes) or
+                                     (route not in assigned_routes and selectable_unassignedroutes))):
                                 continue
                             objlist.append(
                                 ObjectSelectionEntry(obj=obj,
@@ -1010,31 +1010,31 @@ class BolMapViewer(QtOpenGLWidgets.QOpenGLWidget):
                 object_routes = set()
 
                 for camera in self.level_file.cameras:
-                    if camera.route >= 0 and camera in select_optimize:
+                    if camera.route is not None and camera in select_optimize:
                         routes_to_highlight.add(camera.route)
                     camera_routes.add(camera.route)
 
                 for obj in self.level_file.objects.objects:
-                    if obj.pathid >= 0 and obj in select_optimize:
-                        routes_to_highlight.add(obj.pathid)
-                    object_routes.add(obj.pathid)
+                    if obj.route is not None and obj in select_optimize:
+                        routes_to_highlight.add(obj.route)
+                    object_routes.add(obj.route)
 
                 assigned_routes = camera_routes.union(object_routes)
                 shared_routes = camera_routes.intersection(object_routes)
 
-                for i, route in enumerate(self.level_file.routes):
-                    if (not ((i in object_routes and visible_objectroutes) or
-                             (i in camera_routes and visible_cameraroutes) or
-                             (i not in assigned_routes and visible_unassignedroutes))):
+                for route in self.level_file.routes:
+                    if (not ((route in object_routes and visible_objectroutes) or
+                             (route in camera_routes and visible_cameraroutes) or
+                             (route not in assigned_routes and visible_unassignedroutes))):
                         continue
 
-                    selected = i in routes_to_highlight
+                    selected = route in routes_to_highlight
                     route_color = "unassignedroute"
-                    if i in shared_routes:
+                    if route in shared_routes:
                         route_color = "sharedroute"
-                    elif i in object_routes:
+                    elif route in object_routes:
                         route_color = "objectroute"
-                    elif i in camera_routes:
+                    elif route in camera_routes:
                         route_color = "cameraroute"
                     for point in route.points:
                         point_selected = point in select_optimize

widgets/data_editor.py
@@ -237,9 +237,10 @@ class ColorPicker(ClickableLabel):
 class DataEditor(QtWidgets.QWidget):
     emit_3d_update = QtCore.Signal()
 
-    def __init__(self, parent, bound_to):
+    def __init__(self, parent, bol, bound_to):
         super().__init__(parent)
 
+        self.bol = bol
         self.bound_to = bound_to
         self.vbox = QtWidgets.QVBoxLayout(self)
         self.vbox.setContentsMargins(0, 0, 0, 0)
@@ -980,10 +981,18 @@ class ObjectEdit(DataEditor):
         self.objectid = self.add_dropdown_input("Object Type", "objectid", REVERSEOBJECTNAMES)
         self.prev_objectname = None
 
-        self.pathid = self.add_integer_input("Route ID", "pathid",
-                                             MIN_SIGNED_SHORT, MAX_SIGNED_SHORT)
-        set_tool_tip(self.pathid, ttl.objectdata['Route ID'])
-        self.pathid.editingFinished.connect(self.catch_text_update)
+        #self.pathid = self.add_integer_input("Route ID", "pathid",
+        #                                     MIN_SIGNED_SHORT, MAX_SIGNED_SHORT)
+        #set_tool_tip(self.pathid, ttl.objectdata['Route ID'])
+        #self.pathid.editingFinished.connect(self.catch_text_update)
+
+        routes = OrderedDict()
+        routes["None"] = None
+        for i, route in enumerate(self.bol.routes):
+            routes["Route {0}".format(i)] = route
+
+        self.route = self.add_dropdown_input("Route", "route", routes)
+        set_tool_tip(self.route, ttl.objectdata['Route ID'])
 
         self.unk_2a = self.add_integer_input("Route Point ID", "unk_2a",
                                              MIN_SIGNED_SHORT, MAX_SIGNED_SHORT)
@@ -1086,7 +1095,18 @@ class ObjectEdit(DataEditor):
         index = self.objectid.findText(name)
         self.objectid.setCurrentIndex(index)
 
-        self.pathid.setText(str(obj.pathid))
+        #self.pathid.setText(str(obj.pathid))
+
+        try:
+            routeindex = self.bol.routes.index(obj.route)
+        except ValueError:
+            routeindex = -1
+        if routeindex == -1:
+            self.route.setCurrentIndex(0)
+        else:
+            self.route.setCurrentText("Route {0}".format(routeindex))
+
+
         self.unk_2a.setText(str(obj.unk_2a))
         self.presence_filter.set_value(obj.presence_filter)
         self.presence.set_value(obj.presence)
@@ -1181,9 +1201,14 @@ class AreaEdit(DataEditor):
         self.rotation = self.add_rotation_input()
         self.shape = self.add_dropdown_input("Shape", "shape", AREA_SHAPE)
         self.area_type = self.add_dropdown_input("Area Type", "area_type", REVERSE_AREA_TYPES)
-        self.camera_index = self.add_integer_input("Camera Index", "camera_index",
-                                                   MIN_SIGNED_SHORT, MAX_SIGNED_SHORT)
-        set_tool_tip(self.camera_index, ttl.areadata["Camera Index"])
+
+        cameras = OrderedDict()
+        cameras["None"] = None
+        for i, camera in enumerate(self.bol.cameras):
+            cameras["Camera {0}".format(i)] = camera
+        self.camera = self.add_dropdown_input("Camera", "camera", cameras)
+
+        set_tool_tip(self.camera, ttl.areadata["Camera Index"])
         self.feather = self.add_multiple_integer_input("Feather", "feather", ["i0", "i1"],
                                                        MIN_UNSIGNED_INT, MAX_SIGNED_INT)
         for i in self.feather:
@@ -1214,7 +1239,16 @@ class AreaEdit(DataEditor):
 
         self.shape.setCurrentIndex(obj.shape)
         self.area_type.setCurrentIndex(obj.area_type)
-        self.camera_index.setText(str(obj.camera_index))
+
+        try:
+            camindex = self.bol.cameras.index(obj.camera)
+        except ValueError:
+            camindex = -1
+        if camindex == -1:
+            self.camera.setCurrentIndex(0)
+        else:
+            self.camera.setCurrentText("Camera {0}".format(camindex))
+
         self.feather[0].setText(str(obj.feather.i0))
         self.feather[1].setText(str(obj.feather.i1))
         self.unkfixedpoint.setText(str(obj.unkfixedpoint))
@@ -1261,14 +1295,26 @@ class CameraEdit(DataEditor):
         set_tool_tip(self.camduration, ttl.camdata['Camera Duration'])
         self.startcamera = self.add_checkbox("Start Camera", "startcamera", off_value=0, on_value=1)
         set_tool_tip(self.startcamera, ttl.camdata['Start Camera'])
-        self.nextcam = self.add_integer_input("Next Cam", "nextcam",
-                                              MIN_SIGNED_SHORT, MAX_SIGNED_SHORT)
+
+        cameras = OrderedDict()
+        cameras["None"] = None
+        for i, camera in enumerate(self.bol.cameras):
+            cameras["Camera {0}".format(i)] = camera
+
+        self.nextcam = self.add_dropdown_input("Next Cam", "nextcam", cameras)
+
         set_tool_tip(self.nextcam, ttl.camdata['Next Cam'])
         self.shimmer = self.add_multiple_integer_input("Shimmer", "shimmer", ["z0", "z1"], 0, 4095)
-        self.route = self.add_integer_input("Route ID", "route",
-                                            MIN_SIGNED_SHORT, MAX_SIGNED_SHORT)
+
+        routes = OrderedDict()
+        routes["None"] = None
+        for i, route in enumerate(self.bol.routes):
+            routes["Route {0}".format(i)] = route
+
+        self.route = self.add_dropdown_input("Route", "route", routes)
         set_tool_tip(self.route, ttl.camdata['Route ID'])
-        self.route.editingFinished.connect(self.catch_text_update)
+
+        self.route.currentIndexChanged.connect(self.catch_text_update)
         self.routespeed = self.add_integer_input("Route Speed", "routespeed",
                                                  MIN_UNSIGNED_SHORT, MAX_UNSIGNED_SHORT)
         set_tool_tip(self.routespeed, ttl.camdata['Route Speed'])
@@ -1300,9 +1346,27 @@ class CameraEdit(DataEditor):
         self.startcamera.setChecked(obj.startcamera != 0)
         self.shimmer[0].setText(str(obj.shimmer.z0))
         self.shimmer[1].setText(str(obj.shimmer.z1))
-        self.route.setText(str(obj.route))
+
+        try:
+            routeindex = self.bol.routes.index(obj.route)
+        except ValueError:
+            routeindex = -1
+        if routeindex == -1:
+            self.route.setCurrentIndex(0)
+        else:
+            self.route.setCurrentText("Route {0}".format(routeindex))
+
         self.routespeed.setText(str(obj.routespeed))
-        self.nextcam.setText(str(obj.nextcam))
+
+        try:
+            camindex = self.bol.cameras.index(obj.nextcam)
+        except ValueError:
+            camindex = -1
+        if camindex == -1:
+            self.nextcam.setCurrentIndex(0)
+        else:
+            self.nextcam.setCurrentText("Camera {0}".format(camindex))
+
         self.name.setText(obj.name)
 
     def update_name(self):
@@ -1350,6 +1414,7 @@ class RespawnPointEdit(DataEditor):
         if self.bound_to.widget is None:
             return
         self.bound_to.widget.update_name()
+
 
 class LightParamEdit(DataEditor):
     def setup_widgets(self):

widgets/editor_widgets.py
@@ -155,11 +155,11 @@ class ErrorAnalyzer(QtWidgets.QDialog):
                         ))
 
         # Validate path id in objects
-        for object in bol.objects.objects:
-            if object.pathid < -1 or object.pathid + 1 > len(bol.routes):
-                write_line("Map object {0} uses path id {1} that does not exist".format(
-                    get_full_name(object.objectid), object.pathid
-                ))
+        #for object in bol.objects.objects:
+        #    if object.pathid < -1 or object.pathid + 1 > len(bol.routes):
+        #        write_line("Map object {0} uses path id {1} that does not exist".format(
+        #            get_full_name(object.objectid), object.pathid
+        #        ))
 
         # Validate Kart start positions
         if len(bol.kartpoints.positions) == 0:
@@ -183,19 +183,16 @@ class ErrorAnalyzer(QtWidgets.QDialog):
                     exist[kartstartpos.playerid] = True
 
         # Check camera indices in areas
-        for i, area in enumerate(bol.areas.areas):
-            if area.camera_index < -1 or area.camera_index + 1 > len(bol.cameras):
-                write_line("Area {0} uses invalid camera index {1}".format(i, area.camera_index))
+        #for i, area in enumerate(bol.areas.areas):
+        #    if area.camera_index < -1 or area.camera_index + 1 > len(bol.cameras):
+        #        write_line("Area {0} uses invalid camera index {1}".format(i, area.camera_index))
 
         # Check cameras
-        for i, camera in enumerate(bol.cameras):
-            if camera.nextcam < -1 or camera.nextcam + 1 > len(bol.cameras):
-                write_line("Camera {0} uses invalid nextcam (next camera) index {1}".format(
-                    i, camera.nextcam
-                ))
-            if camera.route < -1 or camera.route + 1 > len(bol.routes):
-                write_line("Camera {0} uses invalid path id {1}".format(i,
-                                                                        camera.route))
+        #for i, camera in enumerate(bol.cameras):
+        #    if camera.nextcam < -1 or camera.nextcam + 1 > len(bol.cameras):
+        #        write_line("Camera {0} uses invalid nextcam (next camera) index {1}".format(
+        #            i, camera.nextcam
+        #        ))
 
         # Check respawn points have unique IDs.
         respawn_point_ids = {}
@@ -292,7 +289,6 @@ class AddPikObjectWindow(QtWidgets.QDialog):
         font.setStyleHint(QtGui.QFont.Monospace)
         font.setFixedPitch(True)
         font.setPointSize(10)
-
 
         self.verticalLayout = QtWidgets.QVBoxLayout(self)
         self.verticalLayout.setAlignment(QtCore.Qt.AlignTop)
@@ -483,7 +479,8 @@ class AddPikObjectWindow(QtWidgets.QDialog):
 
             data_editor = choose_data_editor(self.created_object)
             if data_editor is not None:
-                self.editor_widget = data_editor(self, self.created_object)
+                bol = self.parent().level_file
+                self.editor_widget = data_editor(self, bol, self.created_object)
                 self.editor_widget.layout().addStretch()
                 margin = self.fontMetrics().averageCharWidth()
                 self.editor_widget.setContentsMargins(margin, margin, margin, margin)

widgets/more_buttons.py
@@ -60,7 +60,7 @@ class MoreButtons(QtWidgets.QWidget):
             #i suspect that it's useless to add more than 1 point
             optionstrings.append("add_startpoint")
         elif isinstance(obj, MapObject):
-            if obj.route_info() == "Individual" and obj.pathid == -1:
+            if obj.route_info() == "Individual" and obj.route is None:
                 optionstrings.append("route_object")
 
         for name, button in self._buttons.items():

commit f0236085d1ab672ad828c2a8d9990757fb938a02

Preserve assigned cameras when copy/pasting areas.
This was a regression in 016f588, where areas started storing cameras
instances instead of camera indexes.

The pickling mechanism did not work well in this case, as the pasted
areas would present a brand-new camera instance that does not correspond
to any camera in the route list.

Now, before pickling the selected areas into binary data, camera
instances are temporarily converted to camera indexes. During the paste
action, camera indexes are converted to camera instances based on
the current camera list.

mkdd_editor.py
@@ -2558,6 +2558,15 @@ class GenEditor(QtWidgets.QMainWindow):
                     obj.route = self.level_file.routes.index(obj.route)
                 else:
                     obj.route = -1
+        # And the same applies to cameras assigned to areas.
+        areas_cameras = []
+        for obj in self.level_view.selected:
+            if isinstance(obj, libbol.Area):
+                areas_cameras.append((obj, obj.camera))
+                if obj.camera is not None:
+                    obj.camera = self.level_file.cameras.index(obj.camera)
+                else:
+                    obj.camera = -1
 
         try:
             # Effectively serialize the data.
@@ -2569,6 +2578,9 @@ class GenEditor(QtWidgets.QMainWindow):
             # Restore the routes instances.
             for obj, route in object_camera_routes:
                 obj.route = route
+            # Restore the cameras instances.
+            for obj, camera in areas_cameras:
+                obj.camera = camera
 
         mimedata = QtCore.QMimeData()
         mimedata.setData("application/mkdd-track-editor", QtCore.QByteArray(data))
@@ -2629,6 +2641,19 @@ class GenEditor(QtWidgets.QMainWindow):
 
                 target_route.points.append(obj)
 
+                added.append(obj)
+
+        for obj in copied_objects:
+            # Cameras. They may be referenced by other objects; they need to be pasted first.
+            if isinstance(obj, libbol.Camera):
+                if 0 <= obj.route < len(self.level_file.routes):
+                    obj.route = self.level_file.routes[obj.route]
+                else:
+                    obj.route = None
+                self.level_file.cameras.append(obj)
+
+                added.append(obj)
+
         for obj in copied_objects:
             # Group objects.
             if isinstance(obj, libbol.EnemyPointGroup):
@@ -2668,9 +2693,9 @@ class GenEditor(QtWidgets.QMainWindow):
 
             # Autonomous objects.
             elif isinstance(obj, libbol.MapObject):
-                try:
+                if 0 <= obj.route < len(self.level_file.routes):
                     obj.route = self.level_file.routes[obj.route]
-                except IndexError:
+                else:
                     obj.route = None
                 self.level_file.objects.objects.append(obj)
             elif isinstance(obj, libbol.KartStartPoint):
@@ -2682,13 +2707,11 @@ class GenEditor(QtWidgets.QMainWindow):
                 obj.respawn_id = max_respawn_id + 1
                 self.level_file.respawnpoints.append(obj)
             elif isinstance(obj, libbol.Area):
+                if 0 <= obj.camera < len(self.level_file.cameras):
+                    obj.camera = self.level_file.cameras[obj.camera]
+                else:
+                    obj.camera = None
                 self.level_file.areas.areas.append(obj)
-            elif isinstance(obj, libbol.Camera):
-                try:
-                    obj.route = self.level_file.routes[obj.route]
-                except IndexError:
-                    obj.route = None
-                self.level_file.cameras.append(obj)
             elif isinstance(obj, libbol.LightParam):
                 self.level_file.lightparams.append(obj)
             elif isinstance(obj, libbol.MGEntry):