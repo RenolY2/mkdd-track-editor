RenolY2/mkdd-track-editor

commit f095e3145b33b3fda2cfd72b73c7734ba1c69c08

Fix issue with checkpoint selection
It should no longer select both ends when just one end gets selected in 3D view.

mkdd_editor.py
@@ -2587,6 +2587,7 @@ class GenEditor(QMainWindow):
                     for i in range(self.leveldatatreeview.checkpointgroups.childCount()):
                         child = self.leveldatatreeview.checkpointgroups.child(i)
                         item = get_treeitem(child, currentobj)
+
                         if item is not None:
                             break
 
@@ -2608,10 +2609,22 @@ class GenEditor(QMainWindow):
                 elif isinstance(currentobj, libbol.KartStartPoint):
                     item = get_treeitem(self.leveldatatreeview.kartpoints, currentobj)
 
-                #assert item is not None
+                # Temporarily suppress signals to prevent both checkpoints from
+                # being selected when just one checkpoint is selected in 3D view.
+                suppress_signal = False
+                if (isinstance(currentobj, libbol.Checkpoint)
+                    and (currentobj.start in self.level_view.selected_positions
+                         or currentobj.end in self.level_view.selected_positions)):
+                    suppress_signal = True
+
+                if suppress_signal:
+                    self.leveldatatreeview.blockSignals(True)
+
                 if item is not None:
-                    #self._dontselectfromtree = True
                     self.leveldatatreeview.setCurrentItem(item)
+
+                if suppress_signal:
+                    self.leveldatatreeview.blockSignals(False)
 
             #if nothing is selected and the currentitem is something that can be selected
             #clear out the buttons

commit 627bdfc7b6163f0086a7524753235dca2b161be4

Update data editor when checkpoint end is selected.
This was a regression introduced in f095e3145b33.

When one of the two checkpoint ends are selected in the viewport, the
checkpoint was highlighted in the tree view, but the data was not shown
in the data editor due to signal having been temporarily suppressed.

mkdd_editor.py
@@ -2780,9 +2780,9 @@ class GenEditor(QtWidgets.QMainWindow):
                 if suppress_signal:
                     self.leveldatatreeview.blockSignals(False)
 
-            if item is None:
-                # If no item was selected, no tree item will be selected, and the data editor needs
-                # to be updated manually.
+            if item is None or suppress_signal:
+                # If no item was selected, or the signal was suppressed, no tree item will be
+                # selected, and the data editor needs to be updated manually.
                 self.action_update_info()
 
             #if nothing is selected and the currentitem is something that can be selected