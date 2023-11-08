RenolY2/mkdd-track-editor

commit ad7e69babe41daedf94e8d92a0f0234b262df923

Improve camera rendering

mkdd_widgets.py
@@ -809,7 +809,9 @@ class BolMapViewer(QtOpenGLWidgets.QOpenGLWidget):
 
                 if vismenu.cameras.is_selectable():
                     for i, obj in enumerate(obj for obj in self.level_file.cameras if obj not in selected):
-                        if obj in self.selected:
+                        if obj.name == "para":
+                            continue
+                        if obj.camtype in (5, 6):
                             objlist.append(
                                 ObjectSelectionEntry(obj=obj,
                                                      pos1=obj.position,
@@ -820,6 +822,16 @@ class BolMapViewer(QtOpenGLWidgets.QOpenGLWidget):
                                                                                     id + (offset + i) * 4)
                             self.models.render_generic_position_colored_id(obj.position2, id + (offset + i) * 4 + 1)
                             self.models.render_generic_position_colored_id(obj.position3, id + (offset + i) * 4 + 2)
+                        elif obj.camtype == 4:
+                            objlist.append(
+                                ObjectSelectionEntry(obj=obj,
+                                                     pos1=obj.position,
+                                                     pos2=None,
+                                                     pos3=obj.position3,
+                                                     rotation=obj.rotation))
+                            self.models.render_generic_position_rotation_colored_id(obj.position, obj.rotation,
+                                                                                    id + (offset + i) * 4)
+                            self.models.render_generic_position_colored_id(obj.position3, id + (offset + i) * 4 + 1)
                         else:
                             objlist.append(
                                 ObjectSelectionEntry(obj=obj,
@@ -1360,15 +1372,26 @@ class BolMapViewer(QtOpenGLWidgets.QOpenGLWidget):
                     self.models.draw_wireframe_cube(object.position, object.rotation, object.scale*100)
             if vismenu.cameras.is_visible():
                 for object in self.level_file.cameras:
+                    if object.name == "para":
+                        continue
                     self.models.render_generic_position_rotation_colored("camera",
                                                                 object.position, object.rotation,
                                                                  object in select_optimize)
 
-                    if object in select_optimize:
+                    if object.camtype in (4, 5, 6):
                         glColor3f(0.0, 1.0, 0.0)
-                        self.models.draw_sphere(object.position3, 600)
-                        glColor3f(1.0, 0.0, 0.0)
-                        self.models.draw_sphere(object.position2, 600)
+                        self.models.draw_sphere(object.position3, 600, object in select_optimize)
+                        if object.camtype != 4:
+                            glColor3f(1.0, 0.0, 0.0)
+                            self.models.draw_sphere(object.position2, 600, object in select_optimize)
+
+                            glBegin(GL_LINES)
+                            glColor3f(0, 0, 0)
+                            glVertex3f(object.position3.x, -object.position3.z, object.position3.y)
+                            glVertex3f(object.position2.x, -object.position2.z, object.position2.y)
+                            glEnd()
+
+                            self.models.draw_arrow_head(object.position3, (object.position2 + object.position3) / 2)
 
 
             if vismenu.respawnpoints.is_visible():

lib/object_models.py
@@ -43,6 +43,9 @@ class ObjectModels(object):
         with open("resources/unitsphere.obj", "r") as f:
             self.sphere = Model.from_obj(f, rotate=True)
 
+        with open("resources/unitspheresolid.obj", "r") as f:
+            self.sphere_solid = Model.from_obj(f, rotate=True)
+
         with open("resources/unitcylinder.obj", "r") as f:
             self.unitcylinder = Model.from_obj(f, rotate=True)
 
@@ -90,13 +93,16 @@ class ObjectModels(object):
         #glVertex3f(topos.x, -topos.z, topos.y)
         #glEnd()
 
-    def draw_sphere(self, position, scale):
+    def draw_sphere(self, position, scale, solid=False):
         glPushMatrix()
 
         glTranslatef(position.x, -position.z, position.y)
         glScalef(scale, scale, scale)
 
-        self.sphere.render()
+        if solid:
+            self.sphere_solid.render()
+        else:
+            self.sphere.render()
         glPopMatrix()
 
     def draw_sphere_last_position(self, scale):

mkdd_editor.py
@@ -5,7 +5,7 @@ import pstats
 import traceback
 import os
 from timeit import default_timer
-from copy import deepcopy
+from copy import deepcopy, copy
 from io import TextIOWrapper, BytesIO, StringIO
 from math import sin, cos, atan2
 import json
@@ -2728,6 +2728,8 @@ class GenEditor(QtWidgets.QMainWindow):
         if self.level_file is not None:
             item = None
             selected = self.level_view.selected
+            selected_positions = copy(self.level_view.selected_positions)
+
             if len(selected) == 1:
                 currentobj = selected[0]
                 if isinstance(currentobj, libbol.EnemyPoint):
@@ -2776,6 +2778,7 @@ class GenEditor(QtWidgets.QMainWindow):
 
                 if item is not None:
                     self.leveldatatreeview.setCurrentItem(item)
+                    self.level_view.selected_positions = selected_positions
 
                 if suppress_signal:
                     self.leveldatatreeview.blockSignals(False)

resources/unitspheresolid.mtl
@@ -1,0 +1,2 @@
+# Blender 3.6.1 MTL File: 'None'
+# www.blender.org

resources/unitspheresolid.obj
@@ -1,0 +1,211 @@
+# Blender 3.6.1
+# www.blender.org
+mtllib unitspheresolid.mtl
+o Sphere
+v 0.000000 -0.000000 -0.500000
+v 0.000000 -0.433013 -0.250000
+v 0.125000 0.433013 -0.216506
+v 0.216506 0.250000 -0.375000
+v 0.250000 -0.000000 -0.433013
+v 0.216506 -0.250000 -0.375000
+v 0.125000 -0.433013 -0.216506
+v 0.000000 0.500000 -0.000000
+v 0.216506 0.433013 -0.125000
+v 0.375000 0.250000 -0.216506
+v 0.433013 -0.000000 -0.250000
+v 0.375000 -0.250000 -0.216506
+v 0.216506 -0.433013 -0.125000
+v 0.250000 0.433013 -0.000000
+v 0.433013 0.250000 -0.000000
+v 0.500000 0.000000 0.000000
+v 0.433013 -0.250000 0.000000
+v 0.250000 -0.433013 0.000000
+v 0.216506 0.433013 0.125000
+v 0.375000 0.250000 0.216506
+v 0.433013 0.000000 0.250000
+v 0.375000 -0.250000 0.216506
+v 0.216506 -0.433013 0.125000
+v 0.125000 0.433013 0.216506
+v 0.216506 0.250000 0.375000
+v 0.250000 0.000000 0.433013
+v 0.216506 -0.250000 0.375000
+v 0.125000 -0.433013 0.216506
+v 0.000000 0.433013 0.250000
+v 0.000000 0.250000 0.433013
+v 0.000000 0.000000 0.500000
+v 0.000000 -0.250000 0.433013
+v 0.000000 -0.433013 0.250000
+v 0.000000 -0.500000 0.000000
+v -0.125000 0.433013 0.216506
+v -0.216506 0.250000 0.375000
+v -0.250000 0.000000 0.433013
+v -0.216506 -0.250000 0.375000
+v -0.125000 -0.433013 0.216506
+v -0.216506 0.433013 0.125000
+v -0.375000 0.250000 0.216506
+v -0.433013 0.000000 0.250000
+v -0.375000 -0.250000 0.216506
+v -0.216506 -0.433013 0.125000
+v -0.250000 0.433013 -0.000000
+v -0.433013 0.250000 -0.000000
+v -0.500000 0.000000 0.000000
+v -0.433013 -0.250000 0.000000
+v -0.250000 -0.433013 0.000000
+v -0.216506 0.433013 -0.125000
+v -0.375000 0.250000 -0.216506
+v -0.433013 -0.000000 -0.250000
+v -0.375000 -0.250000 -0.216506
+v -0.216506 -0.433013 -0.125000
+v -0.125000 0.433013 -0.216506
+v -0.216506 0.250000 -0.375000
+v -0.250000 -0.000000 -0.433013
+v -0.216506 -0.250000 -0.375000
+v -0.125000 -0.433013 -0.216506
+v 0.000000 0.433013 -0.250000
+v 0.000000 0.250000 -0.433013
+v 0.000000 -0.250000 -0.433013
+vn 0.0692 0.9636 -0.2582
+vn 0.1862 0.6947 -0.6947
+vn -0.0692 0.9636 -0.2582
+vn -0.1862 0.6947 -0.6947
+vn -0.2506 0.2506 -0.9351
+vn -0.6846 0.2506 -0.6846
+vn -0.1890 0.9636 -0.1890
+vn -0.5086 0.6947 -0.5086
+vn -0.2582 0.9636 -0.0692
+vn -0.6947 0.6947 -0.1862
+vn -0.2582 0.9636 0.0692
+vn -0.6947 0.6947 0.1862
+vn -0.1890 0.9636 0.1890
+vn -0.5086 0.6947 0.5086
+vn -0.0692 0.9636 0.2582
+vn -0.1862 0.6947 0.6947
+vn 0.0692 0.9636 0.2582
+vn 0.1862 0.6947 0.6947
+vn 0.1890 0.9636 0.1890
+vn 0.5086 0.6947 0.5086
+vn 0.6845 0.2506 0.6846
+vn 0.2506 0.2506 0.9351
+vn -0.2506 0.2506 0.9351
+vn -0.6846 0.2506 0.6846
+vn -0.9351 0.2506 0.2506
+vn -0.9351 0.2506 -0.2506
+vn -0.9351 -0.2506 -0.2506
+vn -0.6846 -0.2506 -0.6846
+vn -0.2506 -0.2506 -0.9351
+vn 0.2506 -0.2506 -0.9351
+vn 0.2506 0.2506 -0.9351
+vn 0.6845 0.2506 -0.6846
+vn 0.1890 0.9636 -0.1890
+vn 0.5086 0.6947 -0.5086
+vn 0.2582 0.9636 -0.0692
+vn 0.6947 0.6947 -0.1862
+vn 0.2582 0.9636 0.0692
+vn 0.6947 0.6947 0.1862
+vn 0.9351 0.2506 0.2506
+vn 0.9351 0.2506 -0.2506
+vn 0.9351 -0.2506 -0.2506
+vn 0.9351 -0.2506 0.2506
+vn 0.6845 -0.2506 0.6846
+vn 0.2506 -0.2506 0.9351
+vn -0.2506 -0.2506 0.9351
+vn -0.6846 -0.2506 0.6846
+vn -0.9351 -0.2506 0.2506
+vn -0.2582 -0.9636 0.0692
+vn -0.6947 -0.6947 0.1862
+vn -0.1890 -0.9636 0.1890
+vn -0.5086 -0.6947 0.5086
+vn -0.0692 -0.9636 0.2582
+vn -0.1862 -0.6947 0.6947
+vn 0.0692 -0.9636 0.2582
+vn 0.1862 -0.6947 0.6947
+vn 0.1890 -0.9636 0.1890
+vn 0.5086 -0.6947 0.5086
+vn 0.2582 -0.9636 0.0692
+vn 0.6947 -0.6947 0.1862
+vn 0.2582 -0.9636 -0.0692
+vn 0.6947 -0.6947 -0.1862
+vn 0.1890 -0.9636 -0.1890
+vn 0.5086 -0.6947 -0.5086
+vn 0.6845 -0.2506 -0.6846
+vn 0.0692 -0.9636 -0.2582
+vn 0.1862 -0.6947 -0.6947
+vn -0.0692 -0.9636 -0.2582
+vn -0.1862 -0.6947 -0.6947
+vn -0.1890 -0.9636 -0.1890
+vn -0.5086 -0.6947 -0.5086
+vn -0.2582 -0.9636 -0.0692
+vn -0.6947 -0.6947 -0.1862
+s 0
+f 3//1 60//1 8//1
+f 3//2 4//2 61//2 60//2
+f 55//3 8//3 60//3
+f 55//4 60//4 61//4 56//4
+f 61//5 1//5 57//5 56//5
+f 57//6 52//6 51//6 56//6
+f 55//7 50//7 8//7
+f 50//8 55//8 56//8 51//8
+f 50//9 45//9 8//9
+f 50//10 51//10 46//10 45//10
+f 45//11 40//11 8//11
+f 45//12 46//12 41//12 40//12
+f 40//13 35//13 8//13
+f 40//14 41//14 36//14 35//14
+f 35//15 29//15 8//15
+f 35//16 36//16 30//16 29//16
+f 29//17 24//17 8//17
+f 29//18 30//18 25//18 24//18
+f 19//19 8//19 24//19
+f 19//20 24//20 25//20 20//20
+f 20//21 25//21 26//21 21//21
+f 26//22 25//22 30//22 31//22
+f 31//23 30//23 36//23 37//23
+f 37//24 36//24 41//24 42//24
+f 42//25 41//25 46//25 47//25
+f 47//26 46//26 51//26 52//26
+f 47//27 52//27 53//27 48//27
+f 53//28 52//28 57//28 58//28
+f 58//29 57//29 1//29 62//29
+f 62//30 1//30 5//30 6//30
+f 5//31 1//31 61//31 4//31
+f 5//32 4//32 10//32 11//32
+f 3//33 8//33 9//33
+f 3//34 9//34 10//34 4//34
+f 9//35 8//35 14//35
+f 9//36 14//36 15//36 10//36
+f 14//37 8//37 19//37
+f 14//38 19//38 20//38 15//38
+f 20//39 21//39 16//39 15//39
+f 16//40 11//40 10//40 15//40
+f 11//41 16//41 17//41 12//41
+f 17//42 16//42 21//42 22//42
+f 22//43 21//43 26//43 27//43
+f 27//44 26//44 31//44 32//44
+f 32//45 31//45 37//45 38//45
+f 38//46 37//46 42//46 43//46
+f 43//47 42//47 47//47 48//47
+f 44//48 49//48 34//48
+f 49//49 44//49 43//49 48//49
+f 39//50 44//50 34//50
+f 44//51 39//51 38//51 43//51
+f 33//52 39//52 34//52
+f 39//53 33//53 32//53 38//53
+f 28//54 33//54 34//54
+f 33//55 28//55 27//55 32//55
+f 23//56 28//56 34//56
+f 28//57 23//57 22//57 27//57
+f 18//58 23//58 34//58
+f 18//59 17//59 22//59 23//59
+f 18//60 34//60 13//60
+f 13//61 12//61 17//61 18//61
+f 13//62 34//62 7//62
+f 7//63 6//63 12//63 13//63
+f 12//64 6//64 5//64 11//64
+f 7//65 34//65 2//65
+f 2//66 62//66 6//66 7//66
+f 2//67 34//67 59//67
+f 59//68 58//68 62//68 2//68
+f 59//69 34//69 54//69
+f 54//70 53//70 58//70 59//70
+f 54//71 34//71 49//71
+f 49//72 48//72 53//72 54//72

commit c18f84850169a1f37ec562bbb4f9a842707c307c

Avoid skipping enumeration index in camera selection.
Regression in ad7e69babe41daed.

An early-out check was introduced to skip cameras with the `para` name,
altering the enumeration index that is used to access selected objects
later on.

Reproduction steps:

- Add a camera.
- Set its **name** parameter to `para`.
- Add another camera.
- In the viewport, attempt to select the second camera.
- The following exception is produced:

```
Traceback (most recent call last):
  File "/w/mkdd-track-editor/mkdd_widgets.py", line 852, in paintGL
    entry: ObjectSelectionEntry = objlist[index // 4]
IndexError: list index out of range
```

mkdd_widgets.py
@@ -779,9 +779,8 @@ class BolMapViewer(QtOpenGLWidgets.QOpenGLWidget):
                     offset = len(objlist)
 
                 if vismenu.cameras.is_selectable():
-                    for i, obj in enumerate(obj for obj in self.level_file.cameras if obj not in selected):
-                        if obj.name == "para":
-                            continue
+                    for i, obj in enumerate(obj for obj in self.level_file.cameras
+                                            if obj not in selected and obj.name != "para"):
                         if obj.camtype in (5, 6):
                             objlist.append(
                                 ObjectSelectionEntry(obj=obj,