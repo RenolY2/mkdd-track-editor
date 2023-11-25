c89c6a90a1f5c0e5edc1c94b0a9f31a4b1faee9b
cristian64 <cristian64@gmail.com>
2023-11-25 11:51:34 +0000

    Use `QtCore.QKeyCombination` for key sequences that require modifiers.
    Since Python 3, combining `QtCore.Qt.Modifier` enum values with regular
    key values is not supported:
    
    ```
    TypeError: unsupported operand type(s) for |: 'Modifier' and 'Key'
    ```

--- mkdd_editor.py
+++ mkdd_editor.py
@@ -664,7 +664,8 @@ class GenEditor(QtWidgets.QMainWindow):
         self.snapping_toggle_shortcut = QtGui.QShortcut(QtGui.QKeySequence(QtCore.Qt.Key_V), self)
         self.snapping_toggle_shortcut.activated.connect(self.level_view.toggle_snapping)
         self.snapping_cycle_shortcut = QtGui.QShortcut(
-            QtGui.QKeySequence(QtCore.Qt.Key_V | QtCore.Qt.SHIFT), self)
+            QtGui.QKeySequence(QtCore.QKeyCombination(QtCore.Qt.ShiftModifier, QtCore.Qt.Key_V)),
+            self)
         self.snapping_cycle_shortcut.activated.connect(self.level_view.cycle_snapping_mode)
 
         QtGui.QShortcut(QtGui.QKeySequence(QtCore.Qt.Key_G), self).activated.connect(self.action_ground_objects)
@@ -685,7 +686,9 @@ class GenEditor(QtWidgets.QMainWindow):
         self.file_menu = QtWidgets.QMenu(self)
         self.file_menu.setTitle("File")
 
-        save_file_shortcut = QtGui.QShortcut(QtCore.Qt.CTRL | QtCore.Qt.Key_S, self.file_menu)
+        save_file_shortcut = QtGui.QShortcut(
+            QtGui.QKeySequence(QtCore.QKeyCombination(QtCore.Qt.ControlModifier, QtCore.Qt.Key_V)),
+            self.file_menu)
         save_file_shortcut.activated.connect(self.button_save_level)
         #QtGui.QShortcut(QtCore.Qt.CTRL + QtCore.Qt.Key_O, self.file_menu).activated.connect(self.button_load_level)
         #QtGui.QShortcut(QtCore.Qt.CTRL + QtCore.Qt.Key_Alt + QtCore.Qt.Key_S, self.file_menu).activated.connect(self.button_save_level_as)
