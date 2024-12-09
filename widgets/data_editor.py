RenolY2/mkdd-track-editor

commit 6f166010c72c60286b77b0dd93225c512ca760fb

Handle rotation changes in all selected objects.
This was a regression in 7178d896693f42. After the introduction of the
multi-selection data editor, changes in the rotation vectors in the data
editor were ignored, resulting in an occasional runtime warning flagged
by Python when a zero vector was normalized:

```
./lib/vectors.py:25: RuntimeWarning: invalid value encountered in scalar divide
  self.x /= norm
./lib/vectors.py:26: RuntimeWarning: invalid value encountered in scalar divide
  self.y /= norm
./lib/vectors.py:27: RuntimeWarning: invalid value encountered in scalar divide
  self.z /= norm
```

widgets/data_editor.py
@@ -566,7 +566,7 @@ class DataEditor(QtWidgets.QWidget):
         return widget
 
     def update_rotation(self, forwardedits, upedits, leftedits):
-        rotation = get_average_obj(self.bound_to).rotation
+        rotation = self.average_rotation
         forward, up, left = rotation.get_vectors()
 
         for attr in ("x", "y", "z"):
@@ -597,6 +597,7 @@ class DataEditor(QtWidgets.QWidget):
 
     def add_rotation_input(self):
         rotation = get_average_obj(self.bound_to).rotation
+        self.average_rotation = rotation
         forward_spinboxes = []
         up_spinboxes = []
         left_spinboxes = []
@@ -621,6 +622,8 @@ class DataEditor(QtWidgets.QWidget):
             left.normalize()
 
             rotation.set_vectors(newforward, up, left)
+            for obj in self.bound_to:
+                obj.rotation.set_vectors(newforward, up, left)
             self.update_rotation(forward_spinboxes, up_spinboxes, left_spinboxes)
 
         def change_up():
@@ -635,6 +638,8 @@ class DataEditor(QtWidgets.QWidget):
             left.normalize()
 
             rotation.set_vectors(forward, newup, left)
+            for obj in self.bound_to:
+                obj.rotation.set_vectors(forward, newup, left)
             self.update_rotation(forward_spinboxes, up_spinboxes, left_spinboxes)
 
         def change_left():
@@ -650,6 +655,8 @@ class DataEditor(QtWidgets.QWidget):
             up.normalize()
 
             rotation.set_vectors(forward, up, newleft)
+            for obj in self.bound_to:
+                obj.rotation.set_vectors(forward, up, newleft)
             self.update_rotation(forward_spinboxes, up_spinboxes, left_spinboxes)
 
         for edit in forward_spinboxes:

commit 44986d4596ad5075754ecedeb00c4ab97cbe992c

Update rotation widgets in data editor on change.
When the selected object is rotated in the viewport, the rotation fields
in the data editor should now be updated interactively.

This was a regression in 6f166010c72c602, where the object's rotation
was wrongly cached when the widgets are created, totally overlooking
that any potential update coming from the rotate manipulators in the
viewport would be disregarded if the rotation is cached!

widgets/data_editor.py
@@ -651,7 +651,7 @@ class DataEditor(QtWidgets.QWidget):
         return widget
 
     def update_rotation(self, forwardedits, upedits, leftedits):
-        rotation = self.average_rotation
+        rotation = get_average_obj(self.bound_to).rotation
         forward, up, left = rotation.get_vectors()
 
         for attr in ("x", "y", "z"):
@@ -682,7 +682,6 @@ class DataEditor(QtWidgets.QWidget):
 
     def add_rotation_input(self):
         rotation = get_average_obj(self.bound_to).rotation
-        self.average_rotation = rotation
         forward_spinboxes = []
         up_spinboxes = []
         left_spinboxes = []