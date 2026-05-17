b9004db7768f1aeca89bbaab1aaa35f882b9743c
Yoshi2 <RenolY2@users.noreply.github.com>
2026-05-17 13:51:31 +0200

    Version bump

--- mkdd_editor.py
+++ mkdd_editor.py
@@ -57,7 +57,7 @@ from lib.game_visualizer import Game
 from lib.vectors import Vector3
 
 
-__version__ = '1.7.0'
+__version__ = '1.7.0.1'
 
 # These constants will be set by the build script on the fly.
 OFFICIAL = False
